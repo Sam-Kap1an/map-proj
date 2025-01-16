3c30e5a6f8959ce104ee6a5f13840597062e921b
Sam-Kap1an <sfkaplan@calpoly.edu>
2025-01-15 16:48:47 -0800

    please work

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import Map from 'https://cdn.skypack.dev/ol/Map.js';
-import View from 'https://cdn.skypack.dev/ol/View.js';
-import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
-import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-import VectorLayer from 'https://cdn.skypack.dev/ol/layer/Vector.js';
-import VectorSource from 'https://cdn.skypack.dev/ol/source/Vector.js';
-import Feature from 'https://cdn.skypack.dev/ol/Feature.js';
-import Point from 'https://cdn.skypack.dev/ol/geom/Point.js';
-import Style from 'https://cdn.skypack.dev/ol/style/Style.js';
-import CircleStyle from 'https://cdn.skypack.dev/ol/style/Circle.js';
-import Fill from 'https://cdn.skypack.dev/ol/style/Fill.js';
-import Stroke from 'https://cdn.skypack.dev/ol/style/Stroke.js';
-import { fromLonLat } from 'https://cdn.skypack.dev/ol/proj.js';
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import Style from 'ol/style/Style';
+import CircleStyle from 'ol/style/Circle';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import { fromLonLat } from 'ol/proj';
 
 const cities = [
   { coordinates: [-119.731994, 38.937779], count: 1 }, // Nevada
